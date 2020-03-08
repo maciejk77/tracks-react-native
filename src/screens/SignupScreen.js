5dad64427655f6f5c46baf72e4f519c9f3926b94
Maciej Kurek <maciejk77@gmail.com>
2020-03-08 14:52:05 +0000

    Navigation and reusable Context (198 incl)

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
+
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Tracker</Text>
+      </Spacer>
+      <Spacer />
+      <Input
+        label="Email"
+        value={email}
+        onChange={e => setEmail(e.nativeEvent.text)}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer></Spacer>
+      <Input
+        label="Password"
+        value={password}
+        onChange={e => setPassword(e.nativeEvent.text)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      <Spacer>
+        <Spacer />
+        <Button
+          title="Sign Up"
+          onPress={() => navigation.navigate('mainFlow')}
+        />
+      </Spacer>
+    </View>
+  );
+};
+
+SignupScreen.navigationOptions = () => {
+  return { headerShown: false };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 250,
+    //borderColor: 'red',
+    //borderWidth: 10,
+    flex: 1,
+    justifyContent: 'center'
+  }
+});
+
+export default SignupScreen;
